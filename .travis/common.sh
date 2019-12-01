17ed0d98437c2d0e585de3e39bc5d0b8ee36a7ff
Michael Gielda <mgielda@antmicro.com>
2019-12-01 18:57:09 +0100

    __ Add the timvideos channel

--- .travis/common.sh
+++ .travis/common.sh
@@ -61,7 +61,7 @@ fi
 
 export GIT_SSL_NO_VERIFY=1
 export GITREV="$(git describe --long 2>/dev/null || echo "unknown")"
-export CONDA_BUILD_ARGS="$EXTRA_BUILD_ARGS $PACKAGE"
+export CONDA_BUILD_ARGS="$EXTRA_BUILD_ARGS $PACKAGE -c timvideos"
 export CONDA_TEST_ARGS="--test"
 if [ -f "$PACKAGE/conda_build_config.$TOOLCHAIN_ARCH.yaml" ]; then
 	export CONDA_BUILD_ARGS="$CONDA_BUILD_ARGS -m $PACKAGE/conda_build_config.$TOOLCHAIN_ARCH.yaml"
