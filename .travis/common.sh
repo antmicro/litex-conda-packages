b0977395301c99dc11ba6c82d24fc2c9d2e4168b
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2019-12-01 03:17:43 +0100

    [HACK] Failing date command

--- .travis/common.sh
+++ .travis/common.sh
@@ -15,6 +15,7 @@ export -f travis_time_start
 export -f travis_time_finish
 export -f travis_wait
 export -f travis_jigger
+DATE_STR=a
 if [ -z "$DATE_STR" ]; then
 	export DATE_TS="$(git log --format=%ct -n1)"
 	export DATE_NUM="$(date --date=@${DATE_TS} -u +%Y%m%d%H%M%S)"
