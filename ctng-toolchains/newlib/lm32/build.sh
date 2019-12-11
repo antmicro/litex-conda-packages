1a2acd94616c0efcae83462f90adb32a37361caa
Jan Kowalewski <jkowalewski@antmicro.com>
2019-12-11 12:48:49 +0100

    Remove unnecessary build command for ct-ng

--- ctng-toolchains/newlib/lm32/build.sh
+++ ctng-toolchains/newlib/lm32/build.sh
@@ -19,10 +19,6 @@ mkdir "${SRC_DIR}/crosstool-ng/build"
 export PATH="${PATH}:${SRC_DIR}/crosstool-ng/build/bin"
 
 if [[ $OS == "Linux" ]]; then
-    # extract ncurses flags needed to compile ctng
-    export CURSES_LIBS="$(ncursesw6-config --libs)"
-    export CURSES_CFLAGS="$(ncursesw6-config --cflags) -I$(readlink -f $(ncursesw6-config --includedir)/..)"
-    
     # How to build with multiple jobs
     export MAKE_J="nproc"
 elif [[ $OS == "Mac" ]]; then
@@ -31,26 +27,16 @@ elif [[ $OS == "Mac" ]]; then
         cp ${SRC_DIR}/patches/musl/macos/* ${SRC_DIR}/patches/musl/git-ac304227
     fi
 
-    # Set number of max opened files 
+    # Set number of max opened files
     ulimit -n 2048
 
-    # No CURSES_FLAGS
-    export CURSES_FLAGS=""
-
-    # Add include dir to CPATH and choose proper libtool 
+    # Add include dir to CPATH and choose proper libtool
     export CPATH="${SRC_DIR}/../_build_env/include"
     export LIBTOOL="${SRC_DIR}/../_build_env/bin/libtool"
-    
+
     # How to build with multiple jobs
     export MAKE_J="sysctl -n hw.physicalcpu"
 fi
-    
-# Build CT-NG
-./bootstrap
-./configure --prefix="${SRC_DIR}/crosstool-ng/build"
-make -j`${MAKE_J}` CFLAGS="${CURSES_CFLAGS}"
-make install
-cd ..
 
 # Unset these env variables - required by crostool-ng
 unset LD_LIBRARY_PATH LIBRARY_PATH LPATH CPATH C_INCLUDE_PATH CPLUS_INCLUDE_PATH OBJC_INCLUDE_PATH CFLAGS CXXFLAGS CC CXX CPPFLAGS LDFLAGS LDFLAGS_ALL CRT_OBJS LDSO_OBJS LIBC_OBJS CFLAGS_ALL CFLAGS_NOSSP CC LIBS CCAS CCASFLAGS CPP
