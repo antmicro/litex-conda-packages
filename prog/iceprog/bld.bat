749f4f3ed694357e5b2f687b3b294bc4f6447d64
Jakub Jatczak <jjatczak@antmicro.com>
2019-12-12 10:12:23 +0100

    Fix all

--- prog/iceprog/bld.bat
+++ prog/iceprog/bld.bat
@@ -1,10 +1,14 @@
 @echo on
 
-sed -i "s/-ggdb //;" config.mk
-make -j2 CXX="x86_64-w64-mingw32-g++" SUBDIRS="iceprog"
+REM gcc requires path with slashes not backslashes
+python -c "print(r'%PREFIX%'.replace('\\','/'))" > temp.txt
+set /p BASH_PREFIX=<temp.txt
+python -c "print(r'%BUILD_PREFIX%'.replace('\\','/'))" > temp.txt
+set /p BASH_BUILD_PREFIX=<temp.txt
+del temp.txt
+
+make CC="x86_64-w64-mingw32-gcc -I%BASH_BUILD_PREFIX%/Library/include/libftdi1/ -L%BASH_BUILD_PREFIX%/Library/lib" SUBDIRS="iceprog" PREFIX=%BASH_PREFIX%
 if errorlevel 1 exit 1
 mkdir %PREFIX%\bin
-dir iceprog
-cp iceprog\iceprog.exe %PREFIX%\bin
+cp iceprog/iceprog.exe %BASH_PREFIX%/bin/
 if errorlevel 1 exit 1
-
