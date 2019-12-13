752b6b713e7f7e07d84a9f177f899310c2ce22dd
Jakub Jatczak <jjatczak@antmicro.com>
2019-12-13 16:44:15 +0100

    Add iverilog to build

--- eda/sim/icarus/bld.bat
+++ eda/sim/icarus/bld.bat
@@ -1,19 +1,22 @@
-COPY %RECIPE_DIR%\build.sh build.sh
-
-
+ECHO ON
 
 set MSYSTEM=MINGW%ARCH%
+set GCC_ARCH=x86_64-w64-mingw32
+set CC=%GCC_ARCH%-gcc
+set CXX=%GCC_ARCH%-g++
 
-set MSYS2_PATH_TYPE=inherit
-
-set CHERE_INVOKING=1
-
-
+REM gcc requires path with slashes not backslashes
+python -c "print(r'%PREFIX%'.replace('\\','/'))" > temp.txt
+set /p BASH_PREFIX=<temp.txt
 
 bash -lc "ln -s ${LOCALAPPDATA}/Temp /tmp"
-
-bash -lc "./build.sh"
-
+bash -lc "autoconf.sh" 
+if errorlevel 1 exit 1
+bash -lc "./configure --prefix=%BASH_PREFIX%"
+if errorlevel 1 exit 1
+bash -lc "make  -j4"
+if errorlevel 1 exit 1
+bash -lc "make install"
 if errorlevel 1 exit 1
 
 
