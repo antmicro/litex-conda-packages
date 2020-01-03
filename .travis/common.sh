7898ee18746bcb36c34bb10520c541e1369a400b
Jakub Jatczak <jjatczak@antmicro.com>
2020-01-03 16:10:44 +0100

    __ Change timvideos channel to antmicro

--- .travis/common.sh
+++ .travis/common.sh
@@ -55,7 +55,7 @@ fi
 
 export GIT_SSL_NO_VERIFY=1
 export GITREV="$(git describe --long 2>/dev/null || echo "unknown")"
-export CONDA_BUILD_ARGS="$PACKAGE -c timvideos"
+export CONDA_BUILD_ARGS="$PACKAGE -c antmicro"
 if [ -f "$PACKAGE/conda_build_config.$TOOLCHAIN_ARCH.yaml" ]; then
 	export CONDA_BUILD_ARGS="$CONDA_BUILD_ARGS -m $PACKAGE/conda_build_config.$TOOLCHAIN_ARCH.yaml"
 fi
